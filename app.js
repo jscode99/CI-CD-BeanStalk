8194b73b0b5700e310413621248982e6ed2d6a7f
Jishnu Satheesh <34735680+jscode99@users.noreply.github.com>
2023-12-25 22:46:29 +0530

    New api route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/admin", (req, res) => res.send("Bruh, I'm an admin!"));
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
