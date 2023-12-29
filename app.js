bfe83db79692760b8226ee8ff2079850a23aec61
Jishnu Satheesh <34735680+jscode99@users.noreply.github.com>
2023-12-29 17:09:43 +0000

    Module Wrapper & IIFE

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+require('./iife'); // To understand IIFE which works under the hood of the modules in nodejs.
+require('./moduleWrapper'); // To understand the 5 parameters in the module.
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 app.get("/admin", (req, res) => res.send("Bruh, I'm an admin!"));
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
